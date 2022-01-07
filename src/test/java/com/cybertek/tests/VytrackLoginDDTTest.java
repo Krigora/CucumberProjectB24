55b8653147658bf1bcd16ea4258c60789a69a110
krigora <ihor.kryvynskyy@icloud.com>
2022-01-06 21:10:23 -0600

    create new Test packet and create new many Excel class

---
+++ src/test/java/com/cybertek/tests/VytrackLoginDDTTest.java
@@ -0,0 +1,28 @@
+package com.cybertek.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VytrackLoginDDTTest {
+
+    @Before
+    public void setUp(){
+        //set up browser etc if need
+        //open the url
+    }
+
+    @After
+    public void tearDown(){
+        //close browser here
+    }
+
+    @Test
+    public void logginDDTTest(){
+        //open exel file
+        //add page object
+        //loop and read credentials
+        //write the result in excel file
+    }
+
+}
