ac7caef2e2499c8cff5c95395e8840f044f53d41
krigora <ihor.kryvynskyy@icloud.com>
2022-01-27 17:45:46 -0600

    create new scenario to add_spartan.feature and add new pages, stepdef for this scenario

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
-        dryRun = true , //false=> run the test. true => check for missing steps
-        tags = "@Mockaroo"
+        dryRun = false , //false=> run the test. true => check for missing steps
+        tags = "@Span"
 )
 public class CukesRunner {
 }
